b20fec4651c1159ba26fb9f4003ac0213e318c6b
jeong-hyeonsu <51359174+hyeonsuJ@users.noreply.github.com>
2020-12-07 07:54:40 +0900

    Update PushPrint.java

--- Hello World/Hello World/src/PushPrint.java
+++ Hello World/Hello World/src/PushPrint.java
@@ -2,7 +2,6 @@
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("senior");
-		System.out.println("Wankwang Univ.");
 		System.out.println("Jeonbuk Iksan");
 		System.out.println("fighting!");
 	}
